6f375961f91a56fe4259270af2e1899462df0c73
m.chorakchi@ymail.com <m.chorakchi@ymail.com>
2018-12-16 17:10:04 +0330

    interface for JobData

---
+++ src/app/IJobData.ts
@@ -0,0 +1,16 @@
+
+
+export interface IJobData {
+  title: string,
+  description: string,
+  city: string,
+  state: string,
+  created_at: string,
+  end_date: string,
+  id: string,
+  is_awarded: boolean,
+  thumbnail: string,
+  zip_code: string,
+  attachments: Array<string>,
+  counter: Array<string>
+}
