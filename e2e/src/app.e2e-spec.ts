a08c4cb9fd464510e00f70423fac4696a07b8a12
m.chorakchi@ymail.com <m.chorakchi@ymail.com>
2018-12-16 17:20:00 +0330

    e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to myhammer!');
+    expect(page.getTitleText()).toEqual('myhammer');
   });
 });
